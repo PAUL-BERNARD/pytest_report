728afc30fb99a3c75ee99359ebe78c277ffc1f75
PAUL BERNARD <Paul.Bernard@inria.fr>
2026-07-20 15:29:57 +0200

    Add printing limit

--- main.py
+++ main.py
@@ -52,13 +52,20 @@ class TestSuite:
             current_duration = sum(testcase.time for testcase in self.testclasses[testclass])
             print(f" - {testclass:<61}: {current_duration:>10}s ({100*current_duration/total_duration:>5.2f}%)")
 
-    def print_report_class(self, classname):
+    def print_report_class(self, classname, *, limit=0.0):
         tests = self.testclasses[classname]
+        tests = sorted(tests, key=lambda t: t.time, reverse=True)
 
         print(f"{classname}: {len(tests)} tests found")
 
         total_duration = sum(test.time for test in tests)
         for test in tests:
+            if test.time < limit:
+                other_duration = sum(t.time for t in tests[i:])
+                num_other_tests = len(tests)-i
+                print(f" - Other ({num_other_tests} tests)                                              : {other_duration:>10}s ({100*other_duration/total_duration:>5.2f}%) Avg. duration: {other_duration/num_other_tests:.2f}s")
+                return
+
             print(f" - {test.name:<61}: {test.time:>10}s ({100*test.time/total_duration:>5.2f}%)")
 
 def main():
@@ -82,7 +89,7 @@ def main():
         name = testcase.attrib["name"]
         testsuite.add_testcase(classname=classname, name=name, time=time)
 
-    testsuite.print_report_class("deepinv.tests.test_models")
+    testsuite.print_report_class("deepinv.tests.test_models", limit=10.0)
 
 
 if __name__ == "__main__":
